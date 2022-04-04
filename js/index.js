3afa838d1b03c2dbceedd8ccff985519d48881d7
vpham88 <93295690+vpham88@users.noreply.github.com>
2022-04-03 22:02:49 -0400

    Newest Update to JS

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Your index.js file is loaded correctly");
+
+$(".downloadResume").on("click", function(){
+    $(".fadeMe").fadeToggle("slow");
+});
+
+$("letsRole").mouseover(function(){
+    $("letsRole").css("background-color", "white");
+});
